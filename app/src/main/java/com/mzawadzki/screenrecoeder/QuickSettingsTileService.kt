8ccdacc50824ab300c2de48d1016ccf13f8f19e5
mzawadzki0 <macioz5621@gmail.com>
2024-06-29 16:59:09 +0200

    Add QS tile service

---
+++ app/src/main/java/com/mzawadzki/screenrecoeder/QuickSettingsTileService.kt
@@ -0,0 +1,11 @@
+package com.mzawadzki.screenrecoeder
+
+import android.service.quicksettings.TileService
+
+class QuickSettingsTileService : TileService() {
+    override fun onTileAdded() {
+        super.onTileAdded()
+        // TODO
+        println()
+    }
+}
