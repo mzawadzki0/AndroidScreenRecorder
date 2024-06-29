ab91de793ae80ee0e5a540948ee293249cc16023
mzawadzki0 <macioz5621@gmail.com>
2024-06-29 17:23:04 +0200

    Request a permission

--- app/src/main/java/com/mzawadzki/screenrecoeder/MainActivity.kt
+++ app/src/main/java/com/mzawadzki/screenrecoeder/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.mzawadzki.screenrecoeder
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SYSTEM_ALERT_WINDOW)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.SYSTEM_ALERT_WINDOW),
+                0
+            )
+        }
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
