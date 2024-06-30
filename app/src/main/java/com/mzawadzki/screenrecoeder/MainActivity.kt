8adf5690c040fc0cfe06dddee3e11b0d4455fb91
mzawadzki0 <macioz5621@gmail.com>
2024-06-30 16:38:20 +0200

    Entry to overlay permissions

--- app/src/main/java/com/mzawadzki/screenrecoeder/MainActivity.kt
+++ app/src/main/java/com/mzawadzki/screenrecoeder/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.mzawadzki.screenrecoeder
 
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -12,17 +14,24 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.SYSTEM_ALERT_WINDOW)
-            != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(android.Manifest.permission.SYSTEM_ALERT_WINDOW),
-                0
-            )
-        }
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.testButton)
+        button.setOnClickListener {
+            // TODO always false
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.SYSTEM_ALERT_WINDOW
+                )
+                == PackageManager.PERMISSION_GRANTED
+            ) {
+                Toast.makeText(this, "Already granted!", Toast.LENGTH_SHORT).show()
+            } else {
+                startActivity(Intent(android.provider.Settings.ACTION_MANAGE_OVERLAY_PERMISSION))
+            }
+
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
