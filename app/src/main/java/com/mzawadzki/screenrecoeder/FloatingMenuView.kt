c8a86ae2dbc3ca44d29fa8281d0e0c3bd06b5cee
mzawadzki0 <macioz5621@gmail.com>
2024-07-01 18:06:56 +0200

    Add custom view

---
+++ app/src/main/java/com/mzawadzki/screenrecoeder/FloatingMenuView.kt
@@ -0,0 +1,13 @@
+package com.mzawadzki.screenrecoeder
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+
+class FloatingMenuView(context: Context, attributes: AttributeSet) : View(context, attributes) {
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        println()
+    }
+}
